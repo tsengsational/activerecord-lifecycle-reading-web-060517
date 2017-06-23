e028c6e15419a354a632c12d9dcc8fcaaa23080d
github username <github email address>
2017-06-23 13:44:32 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
+
+  before_save :make_title_case
 
   private
 
